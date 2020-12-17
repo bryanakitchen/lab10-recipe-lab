42c6a5ee578a49ff2428ddf383d479e8171b0d8f
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-17 17:23:30 -0500

    join tables

--- __tests__/recipes.test.js
+++ __tests__/recipes.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const Recipe = require('../lib/models/recipe');
+const Log = require('../lib/models/log');
 
 describe('recipe-lab routes', () => {
   beforeEach(() => {
@@ -122,7 +123,8 @@ describe('recipe-lab routes', () => {
       });
   });
 
-  it('Gets a recipe by id', async() => {
+  it('Finds all logs associated with a given Recipe by Recipe ID via GET', async() => {
+
     const recipe = await Recipe.insert({
       name: 'cookies',
       directions: [
@@ -140,28 +142,42 @@ describe('recipe-lab routes', () => {
       ]
     });
 
+    await Promise.all([
+      { dateOfEvent: '2020-10-10', notes: 'Low bake', rating: 4, recipeId: recipe.id },
+      { dateOfEvent: '2020-10-10', notes: 'Low bake', rating: 4, recipeId: recipe.id },
+      { dateOfEvent: '2020-10-10', notes: 'High bake', rating: 4, recipeId: recipe.id }
+    ].map(log => Log.insert(log)));
+
     return request(app)
       .get(`/api/v1/recipes/${recipe.id}`)
-      .then(res => {
+      .then(res => { 
         expect(res.body).toEqual({
-          id: expect.any(String),
-          name: 'cookies',
-          directions: [
-            'preheat oven to 375',
-            'mix ingredients',
-            'put dough on cookie sheet',
-            'bake for 10 minutes'
-          ],  
-          ingredients: [
-            {
-              name: 'flour', 
-              measurement: 'cup', 
-              amount: 1
-            }
-          ]    
+          ...recipe,
+          logs: expect.arrayContaining([
+            { id: '1', dateOfEvent: '2020-10-10', notes: 'Low bake', rating: 4, recipeId: recipe.id },
+            { id: '2', dateOfEvent: '2020-10-10', notes: 'Low bake', rating: 4, recipeId: recipe.id },
+            { id: '3', dateOfEvent: '2020-10-10', notes: 'High bake', rating: 4, recipeId: recipe.id }      
+          ])
         });
       });
   });
+
+  // id: expect.any(String),
+  // name: 'cookies',
+  // directions: [
+  //   'preheat oven to 375',
+  //   'mix ingredients',
+  //   'put dough on cookie sheet',
+  //   'bake for 10 minutes'
+  // ],  
+  // ingredients: [
+  //   {
+  //     name: 'flour', 
+  //     measurement: 'cup', 
+  //     amount: 1
+  //   }
+  // ]    
+
 
   it('Deletes a recipe by id', async() => {
     const recipe = await Recipe.insert({
